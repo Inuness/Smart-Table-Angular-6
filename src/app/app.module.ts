c529f15175cb931f1479eedbef9d350c234dec0c
Inuness <nunenunes.13@gmail.com>
2018-07-20 08:13:33 -0300

    Smart-Table

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
+import { HttpClientModule } from '@angular/common/http';
+import { TableService } from './table/table.service';
+@NgModule({
+  declarations: [
+    AppComponent,
+    TableComponent
+  ],
+  imports: [
+    BrowserModule, Ng2SmartTableModule, HttpClientModule
+  ],
+  providers: [TableService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
